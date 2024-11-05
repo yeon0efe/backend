a2fa8c488e6cc4e34cc5b73a5132a17236f72bfe
yeon0efe <babayeon@gmail.com>
2024-11-05 16:02:24 +0900

    1105

--- 1101/다소니심리상담1/js/main.js
+++ 1101/다소니심리상담1/js/main.js
@@ -21,39 +21,6 @@ var bx = $('.slider').bxSlider({
     }
 });
 
-//#s2
-var a1 =  $('.s2_title img').offset().top;//   '/'
-var a2 = $('.s2_title h2').offset().top; // 제목
-var a3 = $('.s2_title p').offset().top; // 내용
-var a4 = $('.s2_table li').offset().top; //사각박스
-console.log(a1, a2, a3, a4);
-
-$(window).scroll(function(){
-    var sct = $(this).scrollTop();
-
-    if(a1 < sct + 700){
-      $('.s2_title img').addClass('slide');
-    }
-    if(a2 < sct + 700){
-      $('.s2_title h2').addClass('slide');
-    }
-    if(a3 < sct + 700){
-      $('.s2_title p').addClass('slide');
-    }
-    if(a4 < sct + 700){
-      $('.s2_table li').eq(0).addClass('slide');
-      setTimeout(function(){
-        $('.s2_table li').eq(1).addClass('slide');
-      },300);
-      setTimeout(function(){
-        $('.s2_table li').eq(2).addClass('slide');
-      },600);
-      setTimeout(function(){
-        $('.s2_table li').eq(3).addClass('slide');
-      },900);
-    }
-});
-
 
 
 });
