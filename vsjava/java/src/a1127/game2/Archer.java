b57d5f9b39006bf1fa75f9e99c6874e369d16f35
yeon0efe <babayeon@gmail.com>
2024-11-27 17:21:52 +0900

    1127

---
+++ vsjava/java/src/a1127/game2/Archer.java
@@ -0,0 +1,10 @@
+package a1127.game2;
+
+public class Archer extends Hero {
+    public Archer(String name){
+        super(name);
+    }
+    public void fireArrow(){
+        System.out.println("불화살");
+    }
+}
---
+++ vsjava/java/src/a1127/game2/Warrior.java
@@ -0,0 +1,11 @@
+package a1127.game2;
+
+public class Warrior extends Hero {
+   
+    public Warrior(String name){
+        super(name);
+    }
+    public void groundCutting(){
+        System.out.println("대지가르기!");
+    }
+}
